be780e46cb8203b20408cac320abe1e74bafb66f
42ABC <andrew.brady1@richmond.edu>
2023-07-05 17:39:21 -0400

    Start of format code

---
+++ include/yinyang_faithful.h
@@ -0,0 +1,6 @@
+struct yinyang_faithful {
+
+    std::tuple<float*,size_t*,double> cluster(T* v, size_t n, size_t k, size_t d, T* centers, size_t* asg, size_t max_iter, double epsilon) {
+        return 
+    }
+}
---
+++ kmeans.cpp
@@ -0,0 +1,32 @@
+
+//T : the data type of a coordinate of our points
+//initializer: function used to initialize the centers
+//runner: function used to do kmeans (repeated Lloyd's iterations)
+//v: flat array of point coordinates
+//k: number of clusters requested
+//d: dimension of each point
+//centers: flat array of size k*d that we will write the centers into
+//asg: flat array of size n that we will write the point assignments into
+//dist_choice: string representing which distance function we want to use
+//max_iter: maximum number of iterations we will run
+//epsilon: threshold for comparing the new centers to the old centers; if the sum of the distance between the new and old centers is less than epsilon, we stop early
+template<typename T, typename initializer<T>, typename runner<T>>         
+std::tuple<float*,size_t*,double> Kmeans(T* v, size_t n, size_t k, size_t d, float* centers, size_t* asg, std::string dist_choice, size_t max_iter = 1000, double epsilon=0.01) {
+
+    Distance D;
+    if (dist_choice=="euclidean") {
+        if (k >= 36 && d >= 36) {
+            D = EuclideanDistance();
+
+        }
+        else {
+            D = EuclideanDistanceSmall();
+        }
+    }
+    else {
+        std::cout << "Invalid distance choice" << std::endl;
+        abort();
+    }
+    initializer.assign(v,n,k,d,centers,asg,D);
+    runner.cluster(v,n,k,d,centers,asg,D,max_iter,epsilon);
+}
