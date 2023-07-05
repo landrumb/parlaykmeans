94d30fe06be6723d8db9f97740caf61ac3bff8af
Ben Landrum <8enlandrum@gmail.com>
2023-07-05 17:36:36 -0400

    added Points struct

---
+++ include/utils/points.h
@@ -0,0 +1,19 @@
+/* 
+    A struct which wraps a flat array representing points to facilitate access to the points' coordinates.
+ */
+
+template <typename T>
+struct Points {
+    T* points;
+    size_t num_points;
+    size_t dim;
+
+    size_t width = sizeof(T); // width of each dimension in bytes
+
+    Points(T* points, size_t num_points, size_t dim) : points(points), num_points(num_points), dim(dim) {}
+
+    T* operator[](size_t i) {
+        return points + (i * dim) * width;
+    }
+}
+
