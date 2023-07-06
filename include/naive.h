6f5642dedd77a86972dcd0332183686f5a7f7ef8
42ABC <andrew.brady1@richmond.edu>
2023-07-06 09:25:36 -0400

    Added naive implementation, debugging in progress

--- include/naive.h
+++ include/naive.h
@@ -2,14 +2,14 @@
     naive implementation of Lloyd's algorithm for k-means clustering
  */
 
-#include "../parlay/random.h"
-#include "../parlay/parallel.h"
-#include "../parlay/primitives.h"
-#include "../parlay/sequence.h"
-#include "../parlay/slice.h"
-#include "../parlay/delayed.h"
-#include "../parlay/io.h"
-#include "../parlay/internal/get_time.h"
+#include "parlay/random.h"
+#include "parlay/parallel.h"
+#include "parlay/primitives.h"
+#include "parlay/sequence.h"
+#include "parlay/slice.h"
+#include "parlay/delayed.h"
+#include "parlay/io.h"
+#include "parlay/internal/get_time.h"
 
 #include <fcntl.h>
 #include <sys/mman.h>
@@ -20,9 +20,6 @@
 #include <cmath>
 #include <iostream>
 #include <limits>
-#include <set>
-#include <atomic>
-#include <mutex>
 
 template <typename T>
 struct NaiveKmeans {
@@ -38,13 +35,18 @@ struct NaiveKmeans {
     
         point() : best(-1), coordinates(nullptr, nullptr) {
         }
+
+        point(size_t chosen, parlay::slice<T*,T*> coordinates) : best(chosen) {
+          this->coordinates=coordinates;
+
+        }
     };
 
     private: struct center {
         size_t id; // a unique (hopefully) identifier for the center
-        parlay::sequence<T> coordinates; // the pointer to coordinates of the center
+        parlay::sequence<float> coordinates; // the pointer to coordinates of the center
     
-        center(size_t id, parlay::sequence<T> coordinates) : id(id) {
+        center(size_t id, parlay::sequence<float> coordinates) : id(id) {
         
             this->coordinates = coordinates;
         }
@@ -57,15 +59,17 @@ struct NaiveKmeans {
 
     //MUST PASS DISTANCE BY REFERENCE NOT COPY
     //put the coordinates of p onto the stack (in buf) for the calculation
-    private: size_t closest_point_vd(const point<T>& p, sequence<center<float>>& centers, Distance& D) {
+    private: size_t closest_point_vd(const point<T>& p, sequence<center>& 
+    centers, Distance& D) {
     if constexpr(std::is_same<T,float>() == true) {
      
 
           int d = p.coordinates.size();
 
         //no need to convert with a buffer
-        auto distances = parlay::delayed::map(centers, [&](center<float>& q) {
-            return D.distance(p.coordinates.begin(), make_slice(q.coordinates).begin(),d);
+        auto distances = parlay::delayed::map(centers, [&](center& q) {
+            return D.distance(p.coordinates.begin(), 
+              make_slice(q.coordinates).begin(),d);
         });
 
         return min_element(distances) - distances.begin();
@@ -82,7 +86,7 @@ struct NaiveKmeans {
             it += 1; //add 1 for next?
         }
      
-        auto distances = parlay::delayed::map(centers, [&](center<float>& q) {
+        auto distances = parlay::delayed::map(centers, [&](center& q) {
             return D.distance(buf, make_slice(q.coordinates).begin(),d);
         });
 
@@ -98,91 +102,80 @@ struct NaiveKmeans {
 
     }
 
-}
-
-
-//put the coordinates of p onto the stack (in buf) for the calculation
-//purely for debugging purposes, a sequential implementation easier to debug
-template<typename T> size_t closest_point_vd_seq(const point<T>& p, sequence<center<float>>& centers, Distance& D) {
-    if constexpr(std::is_same<T,float>() == true) {
-      assert(centers.size() > 0);
-        assert(p.coordinates.size() == centers[0].coordinates.size());
-
-          int d = p.coordinates.size();
-
-        //no need to convert with a buffer
-        auto distances = parlay::delayed::map(centers, [&](center<float>& q) {
-            return D.distance(p.coordinates.begin(), make_slice(q.coordinates).begin(),d);
-        });
-
-        return min_element(distances) - distances.begin();
-
-        
-    }
-    else {
-        assert(centers.size() > 0);
-        assert(p.coordinates.size() == centers[0].coordinates.size());
-
-        sequence<float> buf1(50,1);
-        sequence<float> buf2(50,2);
-        std::cout << "dist " << D.distance(make_slice(buf1).begin(),make_slice(buf2).begin(),50) << std::endl;
-        std::cout << "finished with dist" << std::endl;
-        int d = p.coordinates.size();
-        float buf[d];
-        T* it = p.coordinates.begin();
-        for (int i = 0; i < d; i++) {
-            buf[i]=*it;
-            it += 1; //add 1 for next?
-        }
-        
-        sequence<float> distances(centers.size());
-        sequence<float> distances2(centers.size());
-        for (int i = 0; i < centers.size(); i++) {
-          // std::cout << "printing out first two points of buffer " << i << std::endl;
-          // std::cout << buf[0] << " " << buf[1] << std::endl;
-          std::cout << "about to call dist" << std::endl;
-          distances[i] = D.distance(buf,make_slice(centers[i].coordinates).begin(),d);
-          std::cout << "just called dist" << std::endl;
-          distances2[i] = euclidean_squared(buf,centers[i].coordinates);
-        }
-        std::cout << "printing different distance calculations: " << std::endl;
-        print_seq(distances);
-        print_seq(distances2);
-     
-        assert(distances==distances2);
-        assert(distances!=distances2);
-        assert(distances[0]==distances2[0]);
-     
-        // auto distances = parlay::delayed::map(centers, [&](center<float>& q) {
-        //     return D.distance(buf, make_slice(q.coordinates).begin(),d);
-        // });
-
-        if (DEBUG_VD) {
-          std::cout << "distance printing" << std::endl;
-          for (int i = 0; i < distances.size(); i++) {
-            std::cout << distances[i] << " " ;
-          }
-          std::cout << std::endl;
-        }
-        return min_element(distances) - distances.begin();
-
-    }
-
-}
+  }
 
 
 //compute centers calculates the new centers
 //returns: a sequence of centers
-template<typename T> sequence<center<float>> compute_centers_vd(const sequence<point<T>>& pts, const sequence<center<float>>& centers, size_t k, size_t d, size_t n) {
+template<typename T> sequence<center<T>> compute_centers_ec(const 
+sequence<point<T>>& pts, size_t n, size_t d, size_t k, const sequence<center<T>>& centers) {
+
+    sequence<sequence<size_t>> indices(k);
+    
+    sequence<center<T>> new_centers(k);
+    for (int i = 0; i < k; i++) {
+        new_centers[i].id = i;
+        new_centers[i].coordinates=sequence<T>(d,4);
+    }
+
+    for (int i = 0; i < n; i++) {
+            
+        indices[pts[i].best].push_back(i); //it's called best not id!!!
+
+    }
+    if (DEBUG_ED) {
+        std::cout << "Debugging: printing out center counts:\n";
+        for (int i = 0; i < k; i++) {
+        std::cout << indices[i].size() << std::endl;
+        }
+
+    }
+    
+    parlay::parallel_for (0, k*d, [&] (size_t icoord){
+        size_t i = icoord / d;
+        size_t coord = icoord % d;
+
+        //std::cout<<"icoord " << icoord << "i : " << i << "coord: " << 
+        //  coord << std::endl;
+        //new_centers[i].coordinates[coord] = 
+        //  anti_overflow_avg(map(new_centers[i].points,[&] 
+        //  (size_t ind) {return pts[ind].coordinates[coord];}  ));
+
+        //if there are no values in a certain center, just keep the center
+        // where it is
+        if (indices[i].size() > 0) { //anti_overflow_avg or reduce??
+            new_centers[i].coordinates[coord] = reduce(map(indices[i],[&] 
+            (size_t ind) {
+              return pts[ind].coordinates[coord];})) 
+              / indices[i].size(); //normal averaging now
+
+        }
+        else { 
+            new_centers[i].coordinates[coord] = centers[i].coordinates[coord];
+        }
+
+    });
+
+
+    return new_centers;
+
+
+}
+
+//compute centers calculates the new centers
+//returns: a sequence of centers
+template<typename T> sequence<center> compute_centers_vd(
+  const sequence<point<T>>& pts, size_t n, size_t d, size_t k, const sequence<center>& centers, 
+  ) {
 
     if constexpr(std::is_same<T,float>() == true) {
         //run the normal compute centers
-        return compute_centers_ec(pts,centers,k,d,n);
+        return compute_centers_ec(pts,n,d,k,centers);
     }
 
     sequence<sequence<size_t>> indices(k);
     
-    sequence<center<float>> new_centers(k);
+    sequence<center> new_centers(k);
     for (int i = 0; i < k; i++) {
         new_centers[i].id = i;
         new_centers[i].coordinates=sequence<float>(d,4);
@@ -208,9 +201,14 @@ template<typename T> sequence<center<float>> compute_centers_vd(const sequence<p
         size_t coord = icoord % d;
 
       
-        //if there are no values in a certain center, just keep the center where it is
-        if (indices[i].size() > 0) { //anti_overflow_avg or reduce?? //note the static cast to double here, because points are whatever
-            new_centers[i].coordinates[coord] = reduce(map(indices[i],[&] (size_t ind) {return static_cast<float>(pts[ind].coordinates[coord]);})) / indices[i].size(); //normal averaging now
+        //if there are no values in a certain center, just keep the center 
+        //where it is
+        if (indices[i].size() > 0) { //anti_overflow_avg or reduce?? 
+        //note the static cast to double here, because points are whatever
+            new_centers[i].coordinates[coord] = reduce(map(indices[i],[&] 
+            (size_t ind) {return static_cast<float>(
+              pts[ind].coordinates[coord]);})) / indices[i].size(); 
+              //normal averaging now
 
         }
         else { 
@@ -226,67 +224,65 @@ template<typename T> sequence<center<float>> compute_centers_vd(const sequence<p
 
 }
 
+template <typename T> void cluster(T* v, size_t n, size_t d, size_t k, 
+float* c, size_t* asg, Distance& D, size_t max_iter, double epsilon) {
+
+  //format the data according to our naive run
+  parlay::sequence<point<T>> pts = parlay::tabulate<point<T>>(n, [&] (size_t i) {
+      return point<T>(asg[i],parlay::slice(v+i*d, v+i*d + d));
+
+  })
+
+  //create the centers
+  parlay::sequence<center> centers = parlay::tabulate<center>(k, [&] (size_t i) {
+    return center(i, parlay::sequence<float>(d));
+  });
+
+  //fill in the centers
+  parlay::parallel_for(0,k, [&] (size_t i) {
+    for (int j = 0; j < d; j++) {
+      centers[i].coordinates[j] = *(c + i*d + j);
+    }
+
+  });
+
+  //the actual naive run
+  kmeans_vd(pts,n,d,k,centers,D,max_iterations,epsilon);
+
+  //put our data back 
+  parlay::parallel_for(0,k,[&] (size_t i)) {
+    for (int j = 0; j < d; j++) {
+      c[i*d + j] = centers[i].coordinates[j];
+    }
+  }
+  parlay::parallel_for(0,n,[&] (size_t i) {
+      asg[i] = pts[i].best;
+  });
+
+}
 
 
 
 
-template <typename T> double kmeans_vd(parlay::sequence<point<T>>& pts, parlay::sequence<center<float>>& centers, size_t k, size_t max_iterations, double epsilon){
+double kmeans_vd(parlay::sequence<point<T>>& pts, size_t n, size_t d, size_t k, 
+parlay::sequence<center>& centers, Distance& D, size_t max_iterations, double epsilon)
+{
 
     std::cout << "running vd" << std::endl;
 
     parlay::internal::timer t = parlay::internal::timer();
     t.start();
     
-
      if(pts.size() == 0){ //default case
         return -1;
     }
 
-    size_t d = pts[0].coordinates.size();
-    size_t n = pts.size();
 
-    assert (k >= 32);
-    assert (d >= 32);
-
-    // if (d <= 32 || k <= 32) { //if d is small, vectorized code doesn't work so run a normal kmeans implementation
-    //     std::cout << "using another type of kmeans run, not vectorization, as the data types aren't applicable" << std::endl;
-    //     //copy the centers to doubles
-    //     //need to properly copy the centers because we are passing by ref
-    //     //FIXME because of the pass by reference the centers (floats) will look unchanged, even though the return value will be correct**
-    //   parlay::sequence<center<double>> centers2(k,center<double>()); 
-    //   for (int i = 0; i < k; i++) {
-    //       centers2[i].id = centers[i].id;
-    //       centers2[i].coordinates=sequence<double>(d);
-    //       for (int j = 0; j < d; j++) {
-    //           centers2[i].coordinates[j] = static_cast<double>(centers[i].coordinates[j]);
-    //       }
-    //   }
-
-    //     return kmeans_double_center(pts,centers2,k,d,n).second;
-
-    // }
-    
-    //if our data is in a double format, just run normal code, don't need a vectorized version
-    if constexpr(std::is_same<T,double>() == true) {
-
-       parlay::sequence<center<double>> centers2(k,center<double>()); 
-        for (int i = 0; i < k; i++) {
-            centers2[i].id = centers[i].id;
-            centers2[i].coordinates=sequence<double>(d);
-            for (int j = 0; j < d; j++) {
-                centers2[i].coordinates[j] = static_cast<double>(centers[i].coordinates[j]);
-            }
-        }
-        return kmeans_euc_dist(pts,centers2,k,d,n);
-    }
-
-    Distance* D = new Euclidian_Distance();
-
-   
-        sequence<float> buf1(50,1);
-        sequence<float> buf2(50,2);
-        std::cout << "dist " << (*D).distance(make_slice(buf1).begin(),make_slice(buf2).begin(),50) << std::endl;
-        std::cout << "finished with dist\n";
+  
+    sequence<float> buf1(50,1);
+    sequence<float> buf2(50,2);
+    std::cout << "dist " << (*D).distance(make_slice(buf1).begin(),make_slice(buf2).begin(),50) << std::endl;
+    std::cout << "finished with dist\n";
   int iterations = 0;
 
   float total_diff = 0;
@@ -334,7 +330,7 @@ template <typename T> double kmeans_vd(parlay::sequence<point<T>>& pts, parlay::
     std::cout << "past closest pt\n";
 
     // Compute new centers
-    sequence<center<float>> new_centers = compute_centers_vd(pts, centers, k, d, n);
+    sequence<center> new_centers = compute_centers_vd(pts, n, d, k, centers);
     t.next("Computed the centers");
     // Check convergence
     total_diff = 0.0;
--- kmeans.cpp
+++ kmeans.cpp
@@ -5,15 +5,18 @@
 //v: flat array of point coordinates
 //k: number of clusters requested
 //d: dimension of each point
-//centers: flat array of size k*d that we will write the centers into
+//c: flat array of size k*d that we will write the centers into
 //asg: flat array of size n that we will write the point assignments into
 //cost: the SSE after the kmeans algorithm finishes
 //dist_choice: string representing which distance function we want to use
 //max_iter: maximum number of iterations we will run
-//epsilon: threshold for comparing the new centers to the old centers; if the sum of the distance between the new and old centers is less than epsilon, we stop early
+//epsilon: threshold for comparing the new centers to the old centers; if the 
+//  sum of the distance between the new and old centers is less than epsilon, 
+//  we stop early
 //returns: a double, the cost of the final center choice
 template<typename T, typename initializer<T>, typename runner<T>>         
-void Kmeans(T* v, size_t n, size_t d, size_t k, float* centers, size_t* asg, std::string dist_choice, size_t max_iter = 1000, double epsilon=0.01) {
+void Kmeans(T* v, size_t n, size_t d, size_t k, float* centers, size_t* asg, 
+std::string dist_choice, size_t max_iter = 1000, double epsilon=0.01) {
 
     Distance D;
     if (dist_choice=="euclidean") {
@@ -30,7 +33,54 @@ void Kmeans(T* v, size_t n, size_t d, size_t k, float* centers, size_t* asg, std
         abort();
     }
 
-    initializer.assign(v,n,k,d,centers,asg,D);
-    runner.cluster(v,n,k,d,centers,asg,cost,D,max_iter,epsilon);
+    Kmeans(v,n,d,k,centers,asg,D,max_iter,epsilon);
+
+}
+
+template<typename T, typename initializer<T>, typename runner<T>>         
+void Kmeans(T* v, size_t n, size_t d, size_t k, float* c, size_t* asg, 
+Distance& dist, size_t max_iter = 1000, double epsilon=0.01) {
+
+    initializer.assign(v,n,d,k,c,asg,D);
+    runner.cluster(v,n,d,k,c,asg,D,max_iter,epsilon);
+
+}
+
+
+//debugging framework until we set up a proper parsing system
+int main() {
+
+    uint8_t* v;
+    size_t n;
+    size_t d;
+    tie(v,n,d) = parse_uint8bin("../Data/base.1B.u8bin.crop_nb_1000");
+    float* c = new float[k*d]; // centers
+    size_t* asg = new size_t[n];
+
+    std::cout << "Printing out initial points: " << std::endl;
+    for (int i = 0; i < n ;i++) {
+        for (int j = 0; j < d; j++) {
+            std::cout << v[i*d+j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    //Kmeans(v,n,d,k,c,asg,"euclidean",10,0.01);
+
+    std::cout << "Printing out final centers: "  << std::endl;
+    for (int i = 0; i < k; i++) {
+        for (int j = 0; j < d; j++) {
+            std::cout << c[i*d + j] <<  " ";
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << "Printing out final assignments: " << std::endl;
+    for (int i = 0; i < 1000; i++) {
+        std::cout << asg[i] << " ";
+    }
+    std::cout << std::endl;
+
+
 
 }
