287054f356ce993967776a810cede8e89597811f
Ben Landrum <8enlandrum@gmail.com>
2023-07-06 15:16:46 -0400

    moved around args

--- include/yinyang_faithful.h
+++ include/yinyang_faithful.h
@@ -1,6 +1,8 @@
+#include <tuple>
+
 struct yinyang_faithful {
 
-    std::tuple<float*,size_t*,double> cluster(T* v, size_t n, size_t k, size_t d, T* centers, size_t* asg, size_t max_iter, double epsilon) {
-        return 
+    std::tuple<float*,size_t*,double> cluster(T* v, size_t n, size_t d, size_t k, T* centers, size_t* asg, size_t max_iter, double epsilon) {
+        return;
     }
-}
+};
