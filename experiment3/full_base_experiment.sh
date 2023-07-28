5831a14d05c50bdbf3701d9829f38a88006b8973
Andrew Brady <acbrady2020@gmail.com>
2023-07-28 09:41:33 -0400

    Posting experiment3 scripts

---
+++ experiment3/full_base_experiment.sh
@@ -0,0 +1,27 @@
+#!bin/bash
+
+#full experiment comparing naive and yy
+echo "Make sure you move any previous experiments out of the output folder first."
+echo "Run experiment? [Y/n]"
+read input 
+if [[ $input == "Y" ]]; then 
+  bazel build kmeans_test_run
+
+  ./bazel-bin/kmeans_test_run -k 10 -i ../my_data/base.1B.u8bin.crop_nb_1000000 -f bin -t uint8 -D short -m 20 -two yes -csv_log true -csv_log_file_name experiment3/test_lazy_naive_10.csv -csv_log_file_name2 experiment3/test_lazy_yy_10.csv
+
+  ./bazel-bin/kmeans_test_run -k 10 -i ../my_data/base.1B.u8bin.crop_nb_1000000 -f bin -t uint8 -D short -m 20 -two yes -csv_log true -csv_log_file_name experiment3/test_lazy_naive_100.csv -csv_log_file_name2 experiment3/test_lazy_yy_100.csv
+
+  ./bazel-bin/kmeans_test_run -k 1000 -i ../my_data/base.1B.u8bin.crop_nb_1000000 -f bin -t uint8 -D short -m 20 -two yes -csv_log true -csv_log_file_name experiment3/test_lazy_naive_1000.csv -csv_log_file_name2 experiment3/test_lazy_yy_1000.csv
+
+  ./bazel-bin/kmeans_test_run -k 10000 -i ../my_data/base.1B.u8bin.crop_nb_1000000 -f bin -t uint8 -D short -m 20 -two yes -csv_log true -csv_log_file_name experiment3/test_lazy_naive_10000.csv -csv_log_file_name2 experiment3/test_lazy_yy_10000.csv
+
+  python include/utils/comparative_time_graphs.py < experiment3/comparative_plot_testing_input.txt
+
+  python include/utils/graph_bench2.py < experiment3/naive_iter_input.txt
+
+  python include/utils/graph_bench2.py < experiment3/yy_iter_input.txt
+
+else
+  echo "Not running the experiment3"
+
+fi
