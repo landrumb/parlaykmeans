e8e970b176a9814042d641df9f13dbc8eb8004d5
Ben Landrum <8enlandrum@gmail.com>
2023-09-09 15:31:11 -0400

    updated path

--- kmeans.sh
+++ kmeans.sh
@@ -2,15 +2,15 @@
 cd ~/parlaykmeans
 bazel build kmeans_test_run
 
-P=/ssd1/data
-G=/home/landrum/outputs
+P=/ssd1/anndata
+# G=/home/landrum/outputs
 O=/ssd1/results
 
 BP=$P/bigann
-# ./bazel-bin/kmeans_test_run -k 1000 -i $BP/base.1B.u8bin.crop_nb_1000000 -f bin -t uint8 -D Euclidean -m 10
+./bazel-bin/kmeans_test_run -k 1000 -i $BP/base.1B.u8bin.crop_nb_1000000 -f bin -t uint8 -D Euclidean -m 10
 
 TP=$P/text2image1B
-./bazel-bin/kmeans_test_run -k 1000 -i $TP/base.1B.fbin.crop_nb_1000000 -f bin -t float -D Euclidean -m 10
+# ./bazel-bin/kmeans_test_run -k 1000 -i $TP/base.1B.fbin.crop_nb_1000000 -f bin -t float -D Euclidean -m 10
 
-#other implementations
-./bazel-bin/kmeans_test_run -k 1000 -i $TP/base.1B.fbin.crop_nb_1000000 -f bin -t float -D Euclidean -m 10 -two yes 
+# #other implementations
+# ./bazel-bin/kmeans_test_run -k 1000 -i $TP/base.1B.fbin.crop_nb_1000000 -f bin -t float -D Euclidean -m 10 -two yes 
